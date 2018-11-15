14c65545045f3dcfedf18a391dc717fbdf42bd7e
sendevman <devman.sd@gmail.com>
2018-11-15 01:31:09 -0700

    ...

--- app/client/src/components/Label.js
+++ app/client/src/components/Label.js
@@ -2,10 +2,10 @@ import React, { Component, Fragment } from "react";
 
 export default class Label extends Component {
     render() {
-        const { label, b } = this.props;
+        const { label, b, labelClass } = this.props;
         return (
             <Fragment>
-                {label ? <label><small className={b ? "form-text text-muted font-weight-bold" : "form-text text-muted"}>{label}</small></label> : null}
+                {label ? <label className={labelClass ? labelClass : undefined}><small className={b ? "form-text text-muted font-weight-bold" : "form-text text-muted"}>{label}</small></label> : null}
             </Fragment>
         )
     }
--- app/client/src/components/ProfileInput.js
+++ app/client/src/components/ProfileInput.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 export default class ProfileInput extends Component {
     render() {
-        const { src } = this.props;
+        const { src, w } = this.props;
         return (
             <div className="text-center">
-                <img src={src} className="rounded-circle" />
+                <img src={src} className="rounded-circle" style={{width:w ? parseInt(w) : undefined }} />
             </div>
         )
     }
