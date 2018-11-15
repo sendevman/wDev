40b339afa777e579af454efd1e4fba6338c43fba
sendevman <devman.sd@gmail.com>
2018-11-15 10:37:29 -0700

    ...

--- app/client/src/components/Input.js
+++ app/client/src/components/Input.js
@@ -10,7 +10,9 @@ export default class Input extends Component {
             <Fragment>
                 {password ?
                     <a style={styles.icon} href="#" onClick={() => this.setState({ revealPass: !this.state.revealPass })}>
-                        <i className={!this.state.revealPass ? "far fa-eye" : "far fa-eye-slash"}></i>
+                        {/* <i className={!this.state.revealPass ? "far fa-eye" : "far fa-eye-slash"}></i> */}
+                        {/* <span class={!this.state.revealPass ? "far eye-f" : "far eye-close-f"}></span> */}
+                        <span className="jam eye-f"></span>
                     </a>
                     : null}
                 <input type={password ? !this.state.revealPass ? "password" : "text" : 'text'} name={name} placeholder={name ? name.charAt(0).toUpperCase() + name.slice(1) : undefined} onChange={onChange} className={`form-control font-weight-bold ${bigSize ? "form-control-lg" : null}`} style={styles.input} autoComplete={disableAutoComplete ? 'on' : 'off'} />
