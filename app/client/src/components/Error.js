9744c7f8aa741e8edc3a05ec8d1e2d4227fd0476
nativelocation <bluesky6460@mail.com>
2018-11-20 16:33:18 -0700

    add more changes on register account

--- app/client/src/components/Error.js
+++ app/client/src/components/Error.js
@@ -9,7 +9,7 @@ import React, { Fragment } from "react";
 const Error = ({ text }) => {
     return (
         <Fragment>
-            {text ? <span className='invalid-feedback'>{text}</span> : undefined}
+            {text ? <span className='invalid-feedback d-block'>{text}</span> : null}
         </Fragment>
     )
 }
