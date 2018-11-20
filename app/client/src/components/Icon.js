c4869c9abf337439a7afa7119f85f05325363586
sendevman <devman.sd@gmail.com>
2018-11-20 12:22:47 -0700

    fixed button in details

---
+++ app/client/src/components/Icon.js
@@ -0,0 +1,16 @@
+import React from "react";
+/**
+ * The Button Component.
+ *
+ * @version 1
+ * @param  name - Add the icon name
+ * @param  size - Set the size 
+ * @param  onClick - Add the method 
+ * @param  fontWeight - Add type of font  
+ */
+const Icon = ({ name, size, onClick, fontWeight, classes }) => {
+    return (
+        <span className={`jam jam-${name} ${classes ? classes : ''}`} style={{ fontSize: size ? parseInt(size) : undefined, fontWeight: fontWeight ? fontWeight : undefined, marginTop: 10 }} onClick={onClick}></span>
+    )
+};
+export default Icon;
