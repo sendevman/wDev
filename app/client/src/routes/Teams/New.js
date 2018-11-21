3afcb276ce7b8af6eb9d77140f0299f870e99de3
nativelocation <bluesky6460@mail.com>
2018-11-21 13:10:49 -0700

    ...

--- app/client/src/routes/Teams/New.js
+++ app/client/src/routes/Teams/New.js
@@ -6,21 +6,32 @@ import Button from '../../components/Button';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import Api from '../../config/api';
+import Error from '../../components/Error';
 
 export default class NewTeam extends Component {
 
   state = { 
-    name: ''
+    name: '',
+    errors: {}
+  }
+
+  onChange(e) {
+    const { name, value } = e.target;
+    let { errors } = this.state;
+    delete errors[name]
+    this.setState({ [name]: value, errors });
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <Wrapper name='Create a new team'>
         <div className='d-flex flex-column'>
           <form>
             <div className="col-md-12">
               <Label label='Name' />
-              <Input name='name' />
+              <Input name='name' onChange={this.onChange.bind(this)} error={!_.isEmpty(errors.name)}/>
+              <Error text={errors.name} />
             </div>
             <hr />
             <div className='mr-3'>
