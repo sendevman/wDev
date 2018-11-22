ac1e4feb421b29e16454c389e335a4332241f4f2
nativelocation <bluesky6460@mail.com>
2018-11-22 12:00:15 -0700

    moving breadcrum to wrapper and fix details

--- app/client/src/components/Wrapper.js
+++ app/client/src/components/Wrapper.js
@@ -1,4 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import Breadcrumb from '../components/Breadcrumb';
+
 /**
  * The Wrapper Component.
  *
@@ -7,16 +9,19 @@ import React, { Component, Fragment } from 'react';
  * @param  children - Add more components 
  */
 
-const Wrapper = ({ children, name }) => {
+const Wrapper = ({ children, name, breadcrumb }) => {
     return (
-        <div id="scrollWrapper" className='col-md-12 col-lg-12 p-3' style={styles.flex}>
-            <div className='col-md-12 col-lg-12 bg-primary m-0 py-2'>
-                <h4 className='font-weight-light pl-2 m-0 text-white'>{name}</h4>
+        <Fragment>
+            <Breadcrumb links={breadcrumb} />
+            <div id="scrollWrapper" className='col-md-12 col-lg-12 p-3' style={styles.flex}>
+                <div className='col-md-12 col-lg-12 bg-primary m-0 py-2'>
+                    <h4 className='font-weight-light pl-2 m-0 text-white'>{name}</h4>
+                </div>
+                <div className='col-md-12 col-lg-12 bg-white p-5'>
+                    {children}
+                </div>
             </div>
-            <div className='col-md-12 col-lg-12 bg-white p-5'>
-                {children}
-            </div>
-        </div>
+        </Fragment>
     );
 }
 
