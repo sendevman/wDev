5b24ea9079707273520a47510ada870f850367b1
sendevman <devman.sd@gmail.com>
2018-11-26 20:11:21 -0700

    changes on loading

--- app/client/src/components/Loading.js
+++ app/client/src/components/Loading.js
@@ -6,9 +6,11 @@ import { COLORS } from '../config/constants';
  * @version 1
  * @param  textColor - Color of text, default is gray
  */
-const Loading = ({ textColor }) => {
+const Loading = ({ show, textColor, absolute }) => {
+    const classAbsolute = absolute ? " position-absolute fixed-top fixed-bottom bg-white" : "";
+    if (!show) return null;
     return (
-        <div className="h-100 d-flex align-items-center justify-content-center">
+        <div className={`h-100 d-flex align-items-center justify-content-center${classAbsolute}`}>
             <div className="d-flex flex-column col-md-3">
                 <object data="/assets/img/loading.svg" />
                 <span className='text-lg-left mx-auto font-weight-light' style={{ color: textColor || COLORS.LightGray, fontSize: 40 }}>LOADING..</span>
