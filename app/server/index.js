8d2b09cd78e67fcbd747e2d144b507ac662e08ba
sendevman <devman.sd@gmail.com>
2019-01-08 14:11:30 -0700

    test

--- app/server/index.js
+++ app/server/index.js
@@ -36,40 +36,39 @@ alexaApp.express({
 });
 
 //Route Name
-const projectRouter = require('./src/routes/project');
-const userRouter = require('./src/routes/user');
+// const projectRouter = require('./src/routes/project');
+// const userRouter = require('./src/routes/user');
 
 const port = process.env.PORT || 8000;
-const publicPath = path.join(__dirname, "../../public");
+// const publicPath = path.join(__dirname, "../../public");
 // Middleware
-app.use(busboy());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.static(publicPath));
-app.use(express.urlencoded({ extended: false }));
-app.use(busboyBodyParser());
+// app.use(busboy());
+// app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(express.static(publicPath));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(busboyBodyParser());
 
 //Routes
-app.use('/project', projectRouter);
-app.use('/user', userRouter);
+// app.use('/project', projectRouter);
+// app.use('/user', userRouter);
 
-app.get("*", (req, res, next) => {
-  console.log('alexa entro')
-  if (req.url.includes("api")) return next();
-  res.sendFile(path.resolve(publicPath, "index.html"));
-});
+// app.get("*", (req, res, next) => {
+//   if (req.url.includes("api")) return next();
+//   res.sendFile(path.resolve(publicPath, "index.html"));
+// });
 
-app.use((req, res, next) => {
-  next(createError(404));
-});
-app.use((err, req, res, next) => {
-  console.log("err.status :", err.status, err);
-  res.status(err.status || 500).json({
-    message: "An error has ocurred.",
-    data: req.app.get("env") === "development" ? err : null
-  });
-});
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
+// app.use((err, req, res, next) => {
+//   console.log("err.status :", err.status);
+//   res.status(err.status || 500).json({
+//     message: "An error has ocurred.",
+//     data: req.app.get("env") === "development" ? err : null
+//   });
+// });
 
-helpers.initApp();
+// helpers.initApp();
 
 app.listen(port, _ => console.log(`The server is listening on port ${port}`));
