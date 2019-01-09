0462146899b8b54b45cd94d235f6f923dff9f683
Angel Ojeda <angel.ojeda.castro@gmail.com>
2019-01-09 16:05:21 -0700

    added project adn people list

--- app/client/src/components/Collapse.js
+++ app/client/src/components/Collapse.js
@@ -13,7 +13,7 @@ class Collapse extends Component {
     e.preventDefault();
     console.log("State ", this.state.value);
     const { onSubmit } = this.props;
-    if(onSubmit) onSubmit('holitas');
+    if (onSubmit) onSubmit('holitas');
   }
 
   onChange(e) {
@@ -21,7 +21,49 @@ class Collapse extends Component {
   }
 
   render() {
-    const {projects, people} = this.props
+    const { projects, people } = this.props
+
+    let projectsName = projects.map((r, i) => {
+      return (
+        <div className="col-md-12 col-lg-12 m-0">
+          <div className="form-check form-check-inline mt-2 ml-2">
+            <input
+              style={styles.checkBoxWidth}
+              className="form-check-input mt-1"
+              type="checkbox"
+              id="inlineCheckbox1"
+              value="option1"
+              checked
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox1" style={styles.checkBoxSize}>
+              {r.name}
+            </label>
+          </div>
+        </div>
+      );
+    });
+
+    let peopleName = people.map((r, i) => {
+      let fullName = r["first-name"] + " " + r["last-name"]
+      return (
+        <div className="col-md-12 col-lg-12 m-0">
+          <div className="form-check form-check-inline mt-2 ml-2">
+            <input
+              style={styles.checkBoxWidth}
+              className="form-check-input mt-1"
+              type="checkbox"
+              id="inlineCheckbox1"
+              value="option1"
+              checked
+            />
+            <label className="form-check-label" htmlFor="inlineCheckbox1" style={styles.checkBoxSize}>
+              {fullName}
+            </label>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <Fragment>
         <form onSubmit={this.onSubmit.bind(this)}>
@@ -29,15 +71,46 @@ class Collapse extends Component {
             Dates:
           </p>
           <div className="col-md-7 d-flex align-items-center ml-2">
-            <SelectInput onChange={this.onChange.bind(this)} value={this.state.value}/>
+            <SelectInput onChange={this.onChange.bind(this)} value={this.state.value} />
           </div>
           <hr />
           <p className="text-white ml-2" style={styles.titleDate}>
             Projects:
+            <div className="col-md-12 col-lg-12 m-0">
+              <div className="form-check form-check-inline mt-2 ml-2">
+                <input
+                  style={styles.checkBoxWidth}
+                  className="form-check-input mt-1"
+                  type="checkbox"
+                  id="inlineCheckbox1"
+                  value="option1"
+                />
+                <label className="form-check-label" htmlFor="inlineCheckbox1" style={styles.checkBoxSize}>
+                  Select None
+                </label>
+              </div>
+            </div>
+            {projectsName}
+            <div className="col-md-12 col-lg-12 m-0">
+              <div className="form-check form-check-inline mt-2 ml-2">
+                <input
+                  style={styles.checkBoxWidth}
+                  className="form-check-input mt-1"
+                  type="checkbox"
+                  id="inlineCheckbox1"
+                  value="option1"
+                  checked
+                />
+                <label className="form-check-label" htmlFor="inlineCheckbox1" style={styles.checkBoxSize}>
+                  Select All
+                </label>
+              </div>
+            </div>
           </p>
           <hr />
           <p className="text-white ml-2" style={styles.titleDate}>
             Developers:
+            {peopleName}
           </p>
           <hr />
           <div className="d-flex flex-row bg-info ml-2">
