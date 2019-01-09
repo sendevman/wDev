d4ba62c7044775749e55901ac12f9c74679795df
nativelocation <bluesky6460@mail.com>
2019-01-09 13:50:16 -0700

    doing real data

--- app/server/src/config/alexa.js
+++ app/server/src/config/alexa.js
@@ -1,29 +1,47 @@
 const alexa = require("alexa-app");
 const { DEBUG } = process.env;
 
+const texts = {
+    welcome: "Welcome to clearview for SerpicoDev, please ask anything you need.",
+    totalHoursResult: "Your total hours for DevView Project is 125hours.",
+    statusUpdateResult: 'Your status update for DevView is "Doing progress on filter data".'
+}
+
+const defaultValues = {
+    slots: {},
+    utterances: []
+}
+
 module.exports = {
     init: (app) => {
         const alexaApp = new alexa.app("alexa");
+
         alexaApp.express({
             expressApp: app,
             checkCert: DEBUG ? false : true,
             debug: DEBUG ? true : false
         });
-        alexaApp.launch(function (request, response) {
-            response.say("You launched the app!");
+
+        alexaApp.launch((req, res) => res.say(texts.welcome));
+
+        alexaApp.intent("TotalHoursIntent", defaultValues, (req, res) => {
+            res.say(texts.totalHoursResult);
         });
 
-        alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
+        alexaApp.intent("StatusUpdateIntent", defaultValues, (req, res) => {
+            res.say(texts.statusUpdateResult);
+        });
 
-        alexaApp.intent("TeamworkHoursIntent", {
-            "slots": { "NAME": "LITERAL" },
-            "utterances": [
-                "my {name is|name's} {names|NAME}", "set my name to {names|NAME}"
-            ]
-        },
-            function (request, response) {
-                response.say("Success!");
-            }
-        );
+        // alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
+        // alexaApp.intent("TeamworkHoursIntent", {
+        //     "slots": { "NAME": "LITERAL" },
+        //     "utterances": [
+        //         "my {name is|name's} {names|NAME}", "set my name to {names|NAME}"
+        //     ]
+        // },
+        //     function (request, response) {
+        //         response.say("Success!");
+        //     }
+        // );
     }
 }
