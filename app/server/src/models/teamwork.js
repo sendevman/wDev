1311a70721e9b170bf137c3ef1ba97ed178528ce
nativelocation <bluesky6460@mail.com>
2019-01-09 01:31:51 -0700

    working on real data on table

--- app/server/src/models/teamwork.js
+++ app/server/src/models/teamwork.js
@@ -13,13 +13,36 @@ const model = {
   getTimeByUser: async data => {
     let { projectId, userId } = data;
     return await tw.projects.getTime(projectId, {
-      userId: userId,
+      // userId: 0,
       fromDate: "20181226",
       toDate: "20181226",
       fromTime: "00:00",
       toTime: "23:59",
       projectType: "active"
     });
+  },
+  getTimeAll: async data => {
+    //User = 150910
+    return await tw.time.get({
+      page: 1,
+      pageSize: 500,
+      fromDate: "20181226",
+      toDate: "20181226",
+      fromTime: "00:00",
+      toTime: "23:59"
+    });
+  },
+  getTotalTimeByDate: async data => {
+    const { userId, fromDate, toDate, fromTime, toTime } = data;
+    return await tw.projects.totalTime(
+      {
+        userId,
+        fromDate,
+        toDate,
+        fromTime,
+        toTime
+      }
+    );
   }
 };
 
