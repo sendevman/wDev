d57b9b8ba201877fb573dc6e39a19050e729fd2b
nativelocation <bluesky6460@mail.com>
2019-01-10 14:00:35 -0700

    ...

--- app/client/src/components/Button.js
+++ app/client/src/components/Button.js
@@ -7,10 +7,11 @@ import { COLORS } from '../config/constants';
  * @param  text - Add the text
  * @param  bigSize - Set the big size
  */
-const Button = ({ text, bigSize, onClick, link }) => {
+const Button = ({ text, bigSize, onClick, link, filter }) => {
     const typeClass = link ? "btn-link text-danger" : "text-white";
     let style = !link ? { ...styles.common } : {};
     if (bigSize) style = { ...style, ...styles.bigButton };
+    if (filter) style = { ...style, ...styles.filter };
     return (
         <button type={`${onClick ? "button" : "submit"}`} onClick={onClick} className={`bg-secondary btn float-right border border-light ${typeClass}`} style={style}>
             {text}
@@ -26,6 +27,10 @@ const styles = {
     bigButton: {
         width: 110,
         height: 50
+    },
+    filter:{
+        width: 100,
+        height: 35
     }
 }
 
