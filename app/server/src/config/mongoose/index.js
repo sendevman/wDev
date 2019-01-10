5125bcd9bab12327e7682dd4c66ae0ad98f86c49
Angel Ojeda <angel.ojeda.castro@gmail.com>
2019-01-10 13:00:40 -0700

    Added model for developer

--- app/server/src/config/mongoose/index.js
+++ app/server/src/config/mongoose/index.js
@@ -4,5 +4,6 @@ const { Schema } = mongoose;
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}).then(() => console.log('DB is Connected')).catch(err => console.error(err));
 
 const User = mongoose.model('User', require('./models/User')(Schema));
+const Developer = mongoose.model('Developer', require('./models/Developer')(Schema));
 
-module.exports = { User };
+module.exports = { User, Developer };
