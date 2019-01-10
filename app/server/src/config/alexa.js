9161525d4d13bc7d8b0b543256f5b50401dc9b78
nativelocation <bluesky6460@mail.com>
2019-01-09 23:04:35 -0700

    making testing

--- app/server/src/config/alexa.js
+++ app/server/src/config/alexa.js
@@ -1,10 +1,12 @@
 const alexa = require("alexa-app");
+const teamwork = require('../models/alexa');
 const { DEBUG } = process.env;
 
 const texts = {
-    welcome: "Welcome to clearview for SerpicoDev, please ask anything you need.",
+    welcome: "Welcome to ClearView for SerpicoDev, you can ask for your status update or what are the total hours in a project.",
     totalHoursResult: "Your total hours for DevView Project is 65.08 hours.",
-    statusUpdateResult: 'Your status update for DevView is "Doing progress on filter data".'
+    statusUpdateResult: 'Your status update for DevView is "Doing progress on filter data".',
+    noIntentFound: "CleaView Can't process that request right now, you can ask for your status update or total hours right now"
 }
 
 const defaultValues = {
@@ -26,6 +28,11 @@ module.exports = {
 
         alexaApp.intent("TotalHoursIntent", defaultValues, (req, res) => {
             console.log('ALEXA TOTALHOURS res :', res);
+            const projectId = 123;
+            teamwork.getTimeAll().then(console.log);
+
+
+
             res.say(texts.totalHoursResult);
         });
 
@@ -35,7 +42,7 @@ module.exports = {
         });
 
 
-        alexaApp.messages.NO_INTENT_FOUND = "CleaView Can't process that request right now, you can ask for your status update or total hours right now";
+        alexaApp.messages.NO_INTENT_FOUND = texts.noIntentFound;
         // alexaApp.dictionary = { "names": ["matt", "joe", "bob", "bill", "mary", "jane", "dawn"] };
         // alexaApp.intent("TeamworkHoursIntent", {
         //     "slots": { "NAME": "LITERAL" },
