070874c2311f8eeb5170ad0935f771fa6e62a028
nativelocation <bluesky6460@mail.com>
2019-01-16 13:42:40 -0700

    fix

--- app/server/src/config/alexa.js
+++ app/server/src/config/alexa.js
@@ -3,6 +3,7 @@ const AmazonSpeech = require('ssml-builder/amazon_speech');
 require('dotenv').config()
 
 const { DEBUG } = process.env;
+const applicationId = 'amzn1.ask.skill.341f13f9-ba93-461c-a9c9-8f261ef6749c';
 
 module.exports = {
     init: (appName, app) => {
@@ -13,6 +14,11 @@ module.exports = {
             checkCert: DEBUG ? false : true,
             debug: DEBUG ? true : false
         });
+
+        alexaApp.pre = request => {
+            const appId = request.sessionDetails.application.applicationId;
+            if (appId !== applicationId) throw 'Invalid application Id: ' + appId;
+        };
 
         return alexaApp;
     },
