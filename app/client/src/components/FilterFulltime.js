1287d360348fcb031fc849506edf6ea98a111313
sendevman <devman.sd@gmail.com>
2019-01-16 18:18:24 -0700

    make devs colors and filter well

--- app/client/src/components/FilterFulltime.js
+++ app/client/src/components/FilterFulltime.js
@@ -1,34 +1,22 @@
 import React, { Component } from "react";
 
 class FilterFulltime extends Component {
-    state = {
-        checkbox0: true,
-        checkbox1: true
-    };
-
-    handleCheckbox(e, type) {
+    handleSelect(e) {
         const { onChange } = this.props;
-        const { checked } = e.target;
-        this.setState({
-            ['checkbox' + type]: checked
-        });
-        if (onChange) onChange(checked, type);
+        const { value } = e.target;
+        if (onChange) onChange(value);
     }
 
     render() {
-        const { checkbox0, checkbox1 } = this.state;
-//do a select instead checkbox
+        //do a select instead checkbox
         return (
-            <div style={{ position: 'absolute', background: 'white', top: 0 }}>
+            <div style={{ position: 'absolute', background: 'white', top: 0, width: '200px' }}>
                 <h6 className="font-weight-light pl-2 m-0">Show:</h6>
-                <div className="form-check form-check-inline mt-2 ml-2">
-                    <input style={styles.checkBoxWidth} className="form-check-input mt-1" type="checkbox" id="checkbox1" checked={checkbox0} onChange={(e) => this.handleCheckbox(e, 0)} />
-                    <label className="form-check-label" htmlFor="checkbox1" style={styles.checkBoxSize}>full-time</label>
-                </div>
-                <div className="form-check form-check-inline">
-                    <input style={styles.checkBoxWidth} className="form-check-input mb-1" type="checkbox" id="checkbox2" checked={checkbox1} onChange={(e) => this.handleCheckbox(e, 1)} />
-                    <label className="form-check-label" htmlFor="checkbox2" style={{ marginTop: -10, fontSize: 12 }}>part-time</label>
-                </div>
+                <select className="custom-select form-control-sm" onChange={this.handleSelect.bind(this)}>
+                    <option value="0">All</option>
+                    <option value="1">FullTime</option>
+                    <option value="2">PartTime</option>
+                </select>
             </div>
         )
     }
