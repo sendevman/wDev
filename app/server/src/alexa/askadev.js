3074c591c5b6442adbdc15ca1884f94b1322951f
nativelocation <bluesky6460@mail.com>
2019-01-18 10:56:27 -0700

    fix app id issues

--- app/server/src/alexa/askadev.js
+++ app/server/src/alexa/askadev.js
@@ -1,10 +1,10 @@
 const alexa = require("../config/alexa");
+const applicationId = 'amzn1.ask.skill.b5133612-ec31-4b22-b1b3-02a56b98ffef';
 
 const texts = {
     welcome: "Welcome to Ask a Developer for SerpicoDev, ",
     ok: 'Okay!'
 }
-
 module.exports = {
     init: (app) => {
         const alexaApp = alexa.init("askadev", app);
