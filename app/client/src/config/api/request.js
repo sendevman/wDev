af30c73a48dc01d95ee5207c040b29c6dfa577ea
sendevman <devman.sd@gmail.com>
2019-01-28 16:45:24 -0700

    making a test

--- app/client/src/config/api/request.js
+++ app/client/src/config/api/request.js
@@ -10,9 +10,9 @@ export function Request(url, object) {
     return new Promise((resolve, reject) => {
         let timer = setTimeout(() => reject(new Error('Request timed out')), FETCH_TIMEOUT);
         fetch(url, object).then(res => res.json())
-            .then(res => resolve(res))
-            .catch(err => reject(err))
-            .finally(() => clearTimeout(timer));
+            .then(res => { clearTimeout(timer); resolve(res) })
+            .catch(err => { clearTimeout(timer); reject(err); })
+        // .finally(() => clearTimeout(timer));
     })
 }
 
@@ -21,6 +21,6 @@ export default function FetchRequest(endpoint, method, token, data, customBase) 
     const headers = Headers(token, customBase);
     const dataParse = data instanceof FormData ? data : JSON.stringify(data);
     const body = method !== 'GET' ? dataParse : null;
-    
+
     return Request(url, { method, headers, body });
 }
