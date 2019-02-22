3aabe00b8e6faa984060d79e52e7328fea3c39a0
nativelocation <bluesky6460@mail.com>
2019-02-22 00:33:19 -0700

    upload changes

--- app/client/src/components/UserGoals.js
+++ app/client/src/components/UserGoals.js
@@ -17,22 +17,24 @@ class UserGoals extends Component {
         return (
             <div className={`d-flex flex-column pl-2 checkedBox ${account._id === user._id ? 'order-0' : ''}`}>
                 <p>{`${user.firstName} ${user.lastName}`}</p>
-                <div className=' pl-2'>
-                    {data.map((x, i) => {
-                        return (
-                            <div className={`d-flex flex-row`} key={i}>
-                                {account._id === x.userId ? (<span className='mr-2'><a href='javascript:;' onClick={() => this.onDelete(x._id)} className='nounderline text-dark'><i className="fa fa-trash-alt" aria-hidden="true"></i></a></span>) : undefined}
-                                <div className="form-check py-1">
-                                    <label className="form-check-label">
-                                        {account._id !== user._id ?
-                                            <input type="checkbox" className="form-check-input" checked={x.checked} readOnly /> :
-                                            <input type="checkbox" className="form-check-input" checked={x.checked} onChange={e => this.onChecked(e, x._id)} />}
-                                        {x.task}
-                                    </label>
-                                </div>
-                            </div>)
-                    })}
-                </div>
+                {data && data.length <= 0 ? <span className='text-muted'>You don't have any task yet.</span> :
+                    <div className='pl-2'>
+                        {data.map((x, i) => {
+                            return (
+                                <div className={`d-flex flex-row`} key={i}>
+                                    {account._id === x.userId ? (<span className='mr-2'><a href='javascript:;' onClick={() => this.onDelete(x._id)} className='nounderline text-dark'><i className="fa fa-trash-alt" aria-hidden="true"></i></a></span>) : undefined}
+                                    <div className="form-check py-1">
+                                        <label className="form-check-label">
+                                            {account._id !== user._id ?
+                                                <input type="checkbox" className="form-check-input" checked={x.checked} readOnly /> :
+                                                <input type="checkbox" className="form-check-input" checked={x.checked} onChange={e => this.onChecked(e, x._id)} />}
+                                            {x.task}
+                                        </label>
+                                    </div>
+                                </div>)
+                        })}
+                    </div>
+                }
             </div>
 
         )
