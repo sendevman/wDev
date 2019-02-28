4a0b84f2b85279a176c46bbfa1b2b9dadba0cf1a
sendevman <devman.sd@gmail.com>
2019-02-27 18:06:30 -0700

    finish socket chat

--- app/server/src/routes/goalSocket.js
+++ app/server/src/routes/goalSocket.js
@@ -1,16 +1,15 @@
 const socket = io => {
   const chat = io.of('/goal_socket');
   chat.on('connection', (socket) => {
-
-    socket.on('JOIN_ROOM', (id) => {
-      console.log('JOIN_ROOM', id);
-      socket.join('ROOM_GOAL');
+    socket.on('GOALCHANGE', (data) => {
+      console.log('GOALCHANGE', data);
+      socket.to('GOAL_ROOM').emit('NEWGOALCHANGE', data);
+    });
+    socket.on('JOINGOALROOM', (data) => {
+      console.log('JOIN_ROOM');
+      socket.join('GOAL_ROOM');
     });
 
-    socket.on('CHANGE', (data) => {
-      console.log('TYPING', data);
-      socket.to('ROOM_GOAL').emit('NEWCHANGE', { data });//not using
-    });
   });
 }
 
