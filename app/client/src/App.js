e1705654f6f7676ea481356cade8ad40cefa30c5
sendevman <devman.sd@gmail.com>
2019-08-16 15:29:34 -0700

    New dashboard added

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -4,13 +4,14 @@ import { Provider } from "react-redux";
 import configureStore from "./redux";
 import Auth from "./config/auth";
 import Login from "./routes/Login";
-import Dashboard from "./routes/Dashboard";
+// import Dashboard from "./routes/Dashboard";
 import Admin from "./routes/Admin";
 import NotFound from "./routes/NotFound";
 import User from "./routes/Users/List";
 import EditUser from "./routes/Users/Edit";
 import Profile from "./routes/Users/Profile";
 import DailyGoals from "./routes/DailyGoals";
+import Dashboard from "./routes/NewDashboard";
 
 const store = configureStore();
 class App extends Component {
@@ -25,7 +26,7 @@ class App extends Component {
                 path="/login"
                 component={e => Auth.validate(Login, e, store)}
               />
-              <Route exact path="/" component={e => Auth.authorize(Dashboard, e, store, 3)} />
+              <Route exact path="/" component={e => Auth.validate(Dashboard, e, store, 3)} />
               <Route exact path="/admin" component={e => Auth.authorize(Admin, e, store, 2)} />
               <Route exact path="/profile" component={e => Auth.authorize(Profile, e, store, 3)} />
               <Route exact path="/user" component={e => Auth.authorize(User, e, store, 1)} />
