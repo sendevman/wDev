2fd17dac5f400cb320d8e7c3f47c39adc1f6efbc
sendevman <devman.sd@gmail.com>
2019-08-19 19:45:32 -0600

    Some URL were updated. Also removed periods

--- app/client/src/routes/NewDashboard.js
+++ app/client/src/routes/NewDashboard.js
@@ -88,7 +88,7 @@ class NewDashboard extends Component {
                                     </a>
                                 </div>
                                 <div>
-                                    <a href="" className="btn btn-lg btn-block grad" target="_blank" style={{ fontSize: "20px", color: "#fff", padding: "2rem", marginBottom: "1rem", textAlign: "left" }}>
+                                    <a href="https://serpicodev-invoice.herokuapp.com" className="btn btn-lg btn-block grad" target="_blank" style={{ fontSize: "20px", color: "#fff", padding: "2rem", marginBottom: "1rem", textAlign: "left" }}>
                                         <h2 style={{ fontFamily: "Raleway" }}>PAYMENT CENTER
                                                 <span className="pull-right" >
                                                 <span className="glyphicon glyphicon-chevron-right"></span>
@@ -115,7 +115,7 @@ class NewDashboard extends Component {
                                     </a>
                                 </div>
                                 <div>
-                                    <a href="#" className="btn btn-lg btn-block grad" style={{ fontSize: "20px", color: "#fff", padding: "2rem", marginBottom: "1rem", textAlign: "left" }}>
+                                    <a href="https://www.wrike.com/form/eyJhY2NvdW50SWQiOjI4NTE4NjUsInRhc2tGb3JtSWQiOjI0Njg3OX0JNDcxOTg2NDIyNTY4MQkwY2I2NTQ2ZjgyNTMyNjU2OTk3MzBlYTVlYzgxNDM0OWNjNWMyN2VjZThlNDY3NGU2Mjk3Njk1MDJhMmI0N2Ix" className="btn btn-lg btn-block grad" style={{ fontSize: "20px", color: "#fff", padding: "2rem", marginBottom: "1rem", textAlign: "left" }}>
                                         <h2 style={{ fontFamily: "Raleway" }}>
                                             REQUEST FOR <br />MORE WORK
                                                 <span className="pull-right">
@@ -135,19 +135,19 @@ class NewDashboard extends Component {
                     <div className="row">
 
                         <div className="col-lg-12 mb-5 mb-lg-0">
-                            <a className="btn btn-outline-light btn-social mx-1" href="#">
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/company/serpico-dev">
                                 <i className="fa fa-lg fa-linkedin"></i>
                             </a>
-                            <a className="btn btn-outline-light btn-social mx-1" href="#">
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://twitter.com/SerpicoDEV">
                                 <i className="fa fa-lg fa-twitter"></i>
                             </a>
-                            <a className="btn btn-outline-light btn-social mx-1" href="#">
+                            <a className="btn btn-outline-light btn-social mx-1" href="mailto:Contact@SerpicoDEV.com">
                                 <i className="fa fa-lg fa-envelope"></i>
                             </a>
                         </div>
 
                         <div className="container" style={{ fontSize: "1.5rem", margin: "3rem", fontFamily: "Raleway", fontStyle: "italic", fontSize: "24px" }}>
-                            <small>Copyrights &copy; 2005-2019 Serpico Enterprises. LLC, Scottsdale, AZ, USA</small>
+                            <small>Copyrights &copy; 2005-2019 Serpico Enterprises. LLC Scottsdale AZ USA</small>
                         </div>
                     </div>
                 </div>
